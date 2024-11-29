9ac42b9a25eec7a727dd2e1df70902301e0743e1
Alex Kremer <akremer@ripple.com>
2024-11-29 14:35:57 +0000

    Fix small issues reported by clang

--- tests/unit/rpc/handlers/LedgerIndexTests.cpp
+++ tests/unit/rpc/handlers/LedgerIndexTests.cpp
@@ -25,7 +25,6 @@
 #include "util/NameGenerator.hpp"
 #include "util/TestObject.hpp"
 
-#include <_stdlib.h>
 #include <boost/json/parse.hpp>
 #include <boost/json/value.hpp>
 #include <gmock/gmock.h>
