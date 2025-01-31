3c42f860a3d4a4b334266e27c2364ec2ffe43254
Alex Kremer <akremer@ripple.com>
2025-01-31 00:17:32 +0000

    Try to make things build

--- cmake/Settings.cmake
+++ cmake/Settings.cmake
@@ -23,8 +23,6 @@ set(COMPILER_FLAGS
     -Wno-null-dereference
     -Wno-maybe-uninitialized
     -Wno-unknown-warning-option # and this to work with clang
-    -Wno-error=tsan # tsan warns about atomic_thread_fence in boost
-    -Wno-tsan
     # TODO: Address these and others in https://github.com/XRPLF/clio/issues/1273
 )
 
@@ -39,6 +37,28 @@ endif ()
 
 if (is_appleclang)
   list(APPEND COMPILER_FLAGS -Wreorder-init-list)
+endif ()
+
+if (san)
+  # for the time being we want the builds to succeed and sanitizers to assess our runtime behaviour
+  list(
+    APPEND
+    COMPILER_FLAGS
+    -Wno-error=tsan # Disables treating TSAN warnings as errors
+    -Wno-tsan # Disables TSAN warnings (thread-safety analysis)
+    -Wno-uninitialized # Disables warnings about uninitialized variables (AddressSanitizer, UndefinedBehaviorSanitizer,
+                       # etc.)
+    -Wno-stringop-overflow # Disables warnings about potential string operation overflows (AddressSanitizer)
+    -Wno-unsafe-buffer-usage # Disables warnings about unsafe memory operations (AddressSanitizer)
+    -Wno-frame-larger-than # Disables warnings about stack frame size being too large (AddressSanitizer)
+    -Wno-unused-function # Disables warnings about unused functions (LeakSanitizer, memory-related issues)
+    -Wno-unused-but-set-variable # Disables warnings about unused variables (MemorySanitizer)
+    -Wno-memset-zero # Disables warnings about unnecessary memset to zero (MemorySanitizer)
+    -Wno-mutex # Disables warnings related to mutex usage (ThreadSanitizer)
+    -Wno-sign-compare # Disables warnings about signed/unsigned comparison (UndefinedBehaviorSanitizer)
+    -Wno-nonnull # Disables warnings related to null pointer dereferencing (UndefinedBehaviorSanitizer)
+    -Wno-address # Disables warnings about address-related issues (UndefinedBehaviorSanitizer)
+  )
 endif ()
 
 # See https://github.com/cpp-best-practices/cppbestpractices/blob/master/02-Use_the_Tools_Available.md#gcc--clang for
