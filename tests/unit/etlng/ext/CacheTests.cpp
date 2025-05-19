5688081a5e636c586be2df0816b92c7a14ac92a7
Alex Kremer <akremer@ripple.com>
2025-05-19 15:59:12 +0100

    Fix cache test with readonly

--- tests/unit/etlng/ext/CacheTests.cpp
+++ tests/unit/etlng/ext/CacheTests.cpp
@@ -21,6 +21,7 @@
 #include "etlng/impl/CacheUpdater.hpp"
 #include "etlng/impl/ext/Cache.hpp"
 #include "util/BinaryTestObject.hpp"
+#include "util/LoggerFixtures.hpp"
 #include "util/MockLedgerCache.hpp"
 #include "util/MockPrometheus.hpp"
 #include "util/TestObject.hpp"
@@ -58,7 +59,7 @@ createTestData()
 
 }  // namespace
 
-struct CacheExtTests : util::prometheus::WithPrometheus {
+struct CacheExtTests : NoLoggerFixture, util::prometheus::WithPrometheus {
 protected:
     MockLedgerCache cache_;
     std::shared_ptr<etlng::impl::CacheUpdater> updater_ = std::make_shared<etlng::impl::CacheUpdater>(cache_);
@@ -91,4 +92,9 @@ TEST_F(CacheExtTests, OnInitialObjectsUpdateCache)
     EXPECT_CALL(cache_, update(objects, kSEQ));
 
     ext_.onInitialObjects(kSEQ, objects, kUNUSED_LAST_KEY);
+}
+
+TEST_F(CacheExtTests, AllowInReadonlyReturnsTrue)
+{
+    EXPECT_TRUE(ext_.allowInReadonly());
 }
