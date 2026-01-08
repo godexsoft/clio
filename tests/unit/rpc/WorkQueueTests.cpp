b91a2c47ce1817b41b0bed106df125fe8be9146f
Alex Kremer <akremer@ripple.com>
2026-01-08 15:49:29 +0000

    Fix test

--- tests/unit/rpc/WorkQueueTests.cpp
+++ tests/unit/rpc/WorkQueueTests.cpp
@@ -225,7 +225,6 @@ TEST_F(WorkQueueMockPrometheusTest, postCoroCounters)
     EXPECT_CALL(queuedMock, add(1));
     EXPECT_CALL(durationMock, add(::testing::Ge(0))).WillOnce([&](auto) {
         EXPECT_CALL(curSizeMock, add(-1));
-        EXPECT_CALL(curSizeMock, value()).WillOnce(::testing::Return(0));
         semaphore.release();
     });
 
