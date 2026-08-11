2601f8d688490ce43d630557bfa3c0da701b75dd
Alex Kremer <akremer@ripple.com>
2026-08-11 18:13:47 +0100

    Introduce rpcspec 0.1.3 dependency

--- conanfile.py
+++ conanfile.py
@@ -17,6 +17,7 @@ class ClioConan(ConanFile):
         "fmt/12.1.0",
         "libbacktrace/cci.20210118",
         "spdlog/1.17.0",
+        "xrpl-rpc-spec/0.1.3",
         "xrpl/3.3.0-rc1-custom",
     ]
 
